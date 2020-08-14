7766d54a4e0ee5010af67d40075976d3dd82e825
Erik Wahlström <erik.wahlstrom@calabrio.com>
2020-08-14 17:17:43 +0200

    Updates

---
+++ precache-manifest.67f4295e1e63eb4a997317bb174ed390.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae9f03dfa0cd5d0e06e27fa3e8b05b93",
+    "url": "/sessionanalyzer/index.html"
+  },
+  {
+    "revision": "cd58f0ade672a4e310a6",
+    "url": "/sessionanalyzer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2ef129499f6873052742",
+    "url": "/sessionanalyzer/static/js/2.7151486f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sessionanalyzer/static/js/2.7151486f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd58f0ade672a4e310a6",
+    "url": "/sessionanalyzer/static/js/main.09aacd6a.chunk.js"
+  },
+  {
+    "revision": "d9c785cafee377e5bf94",
+    "url": "/sessionanalyzer/static/js/runtime-main.da9f3ef8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sessionanalyzer/static/media/logo.5d5d9eef.svg"
+  }
+]);
